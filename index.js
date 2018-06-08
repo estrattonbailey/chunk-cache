ab2aa271597a271bcdb5871c66c8e3336e97dd61
estrattonbailey <ericstrattonbailey@gmail.com>
2018-06-08 11:53:39 -0400

    Don't cache if maxAge === 0

--- index.js
+++ index.js
@@ -2,22 +2,20 @@ const { Transform } = require('stream')
 const memory = require('memory-cache')
 
 module.exports = function cache (key, maxAge) {
-  if (!maxAge) {
-    return memory.get(key)
-  } else if (typeof maxAge !== 'number') {
+  if (typeof maxAge !== 'number') {
     throw new Error(`maxAge must be a number (in ms)`)
-  } else {
-    const chunks = []
-
-    return new Transform({
-      transform (data, enc, cb) {
-        chunks.push(data)
-        cb(null, data)
-      },
-      flush (cb) {
-        memory.put(key, Buffer.concat(chunks), maxAge)
-        cb()
-      }
-    })
   }
+
+  const chunks = []
+
+  return new Transform({
+    transform (data, enc, cb) {
+      maxAge && chunks.push(data)
+      cb(null, data)
+    },
+    flush (cb) {
+      maxAge && memory.put(key, Buffer.concat(chunks), maxAge)
+      cb()
+    }
+  })
 }
