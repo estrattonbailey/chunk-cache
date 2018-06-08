79dfc82fa94ed3b9e9a614b8818a4857a4066e56
estrattonbailey <ericstrattonbailey@gmail.com>
2018-06-08 12:23:57 -0400

    Fix getter

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const { Transform } = require('stream')
 const memory = require('memory-cache')
 
-module.exports = function cache (key, maxAge) {
-  if (typeof maxAge !== 'number') {
-    throw new Error(`maxAge must be a number (in ms)`)
-  }
-
+function stream (key, maxAge) {
   const chunks = []
 
   return new Transform({
@@ -18,4 +14,16 @@ module.exports = function cache (key, maxAge) {
       cb()
     }
   })
+}
+
+module.exports = function cache (key, maxAge) {
+  if (maxAge === 0) {
+    return stream(key, maxAge)
+  } else if (!maxAge) {
+    return memory.get(key)
+  } else if (typeof maxAge !== 'number') {
+    throw new Error(`maxAge must be a number (in ms)`)
+  }
+
+  return stream(key, maxAge)
 }
